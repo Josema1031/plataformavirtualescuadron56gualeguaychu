a1ffbef7f3cd8b245440ee49a3cc82008410ea42
jose carrizo <santinodionel02@gmail.com>
2025-04-30 16:34:50 -0300

    agregacion de cards en archivo noticias

--- script.js
+++ script.js
@@ -1,20 +1,36 @@
-// Obtener los elementos del DOM
-var modal = document.getElementById("myModal");
-var span = document.getElementsByClassName("close")[0];
+window.onload = function () {
+    // Modal
+    var modal = document.getElementById("myModal");
+    var span = document.getElementsByClassName("close")[0];
 
-// Abrir la ventana modal automáticamente al cargar la página
-window.onload = function() {
-    modal.style.display = "block";
-}
+    if (modal && span) {
+        // Mostrar modal al cargar
+        modal.style.display = "block";
 
-// Cuando el usuario hace clic en la X, cierra la ventana modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
+        // Cerrar modal al hacer clic en la X
+        span.onclick = function () {
+            modal.style.display = "none";
+        };
 
-// Cuando el usuario hace clic fuera de la ventana modal, cierra la ventana modal
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+        // Cerrar modal si se hace clic fuera de él
+        window.onclick = function (event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        };
     }
-}
+
+    // Menú hamburguesa
+    const hamburger = document.getElementById("hamburger");
+    const menuLinks = document.getElementById("menu-links");
+
+    if (hamburger && menuLinks) {
+        hamburger.addEventListener("click", () => {
+            menuLinks.classList.toggle("active");
+        });
+    }
+};
+
+
+
+
